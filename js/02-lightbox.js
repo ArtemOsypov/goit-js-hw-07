8fcddde1f80ad4de769f541f46bf21fbd4060306
ArtemOsypov <artem.osypov@gmail.com>
2023-03-09 00:04:37 +0200

    #6

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,32 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galary = document.querySelector(".gallery");
+const imageItem = createGalaryItem(galleryItems);
+galary.insertAdjacentHTML("beforeend", imageItem);
+// const items = document.querySelectorAll("gallery__item");
+galary.addEventListener("click", onGalaryClick);
+
+function createGalaryItem(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+    })
+    .join("");
+}
+function onGalaryClick(evt) {
+  evt.preventDefault();
+}
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionDelay: 250,
+  captionSelector: "img",
+  captionsData: "alt",
+  captionPosition: "bottom",
+});
